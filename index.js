1a4608b4f7db9f132777529443b287cd8f466f67
Sergio Sepulveda <contacto@penti2s.com>
2022-08-16 13:26:14 -0400

    add endpoint registro usuario

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bcryppt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const axios = require('axios').default;
+const User = require('./Models/user');
 require('dotenv').config();
 
 mongoose.connect(process.env.URL_DB)
@@ -10,3 +12,27 @@ const app = express();
 
 app.use(express.json());
 
+app.post('/register', async (req, res) => {
+    const { body } = req;
+    try {
+        const usuario = await User.findOne({ email: body.email });
+        if (usuario) {
+            res.status(400).send({ message: 'El usuario ya existe' });
+        } else {
+            const salt = await bcryppt.genSalt();
+            const hashed = await bcryppt.hash(body.password, salt);
+            const userCreated = await User.create({ email: body.email, password: hashed, salt });
+            jwt.sign({ _id: userCreated._id }, process.env.JWT_SECRET)
+
+            res.send({ _id: userCreated._id })
+        }
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).send(error.message);
+    }
+})
+
+app.listen(3000, ()=>{
+    console.log('Server on port 3000');
+})
